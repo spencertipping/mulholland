93368ba3bc62703c3b18c04071a5d0a8106145b4
Spencer Tipping <spencer@spencertipping.com>
2012-01-21 15:09:57 -0700

    The rewriter now mostly works. It has a few cases where it gives up too early / fails to find a solution, but overall it rewrites things successfully.

--- src/mh/mh.js
+++ src/mh/mh.js
@@ -12,6 +12,5 @@ return(function( ) {var evaluator=function(s,_1,_2,cc) {;
 return(function( ) {try{return(cc(null,$.mulholland.mh(s) .structure() ) ) }catch(e) {return(cc(e,undefined) ) } } ) .call(this) } ;
 return(require( 'repl' ) .start( 'mh> ' ,undefined,evaluator) ) } ) .call(this) } ,$.mulholland.mh=function(source) {;
 return(function( ) {var parse=$.mulholland() ,rewrite_form=parse( '_x //@ [_y]' ) ,rewrite=function(match) {;
-return match&&$.mulholland.rewriter(match._y.flatten( ',' ) ) (match._x) } ,toplevel_rewriter=$.mulholland.rewriter( [ [rewrite_form,rewrite] ] ) ,fixed_point_rewrite=function(t) {;
-return t.dfs(rewrite_form,function(_) {return(function(it) {return(it&&fixed_point_rewrite(toplevel_rewriter(it._) ) ) } ) .call(this, ( (_) .match(this) ) ) } ) ||t} ;
-return(fixed_point_rewrite(parse(source) ) ) } ) .call(this) } } ) ;
+return match&&$.mulholland.rewriter(match._y.flatten( ',' ) ) (match._x) } ,toplevel_rewriter=$.mulholland.rewriter( [ [rewrite_form,rewrite] ] ) ;
+return(toplevel_rewriter(parse(source) ) ) } ) .call(this) } } ) ;
