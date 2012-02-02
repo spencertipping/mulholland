00fa31647b836162203d084ce612336e03c090d3
Spencer Tipping <spencer@spencertipping.com>
2012-02-01 18:44:34 -0700

    Fixed associativity of $, which may end up being a mistake but makes life easier for the moment (it creates an associativity collision, which may cause problems).

--- src/i/parse.js
+++ src/i/parse.js
@@ -18,7 +18,7 @@ return Object.prototype.hasOwnProperty.call(table,n) ?table[n] :n} ,precedence_t
 for(var xr=new xs.constructor() ,xi=0,xl=xs.length;
 xi<xl;
  ++xi)x=xs[xi] ,xr.push( (new RegExp( ( '^' + (x) + '' ) ) ) ) ;
-return xr} ) .call(this, [ '\\.' , '#' , '[~!]' , '[/*]' , '[%`]' , '[+-]' , '[\\\\:]' , '[<>]' , '[&?]' , '[(\\[{]' , '[}\\])]' , '\\^' , '=' , '\\|' , ',' , ';' ] ) ,base_precedence=function(t) {;
+return xr} ) .call(this, [ '\\.' , '#' , '[~!]' , '[/*]' , '[%`]' , '[-+]' , '[\\\\:]' , '[<>]' , '[&?]' , '[(\\[{]' , '[}\\])]' , '\\^' , '=' , '\\|' , ',' , ';' ] ) ,base_precedence=function(t) {;
 return(function(xs) {var x,x0,xi,xl,xr;
 for(var x=xs[0] ,xi=0,xl=xs.length,x1;
 xi<xl;
@@ -27,9 +27,10 @@ if(x1= (x.test(t) &&xi+2) )return x1}return false} ) .call(this,precedence_table
 return is_value(t) ? {l:0,r:0,u:0,id:intern(t) ,v:true,u:false,i:false} :parse_operator(t) } ,parse_operator=function(t) {;
 return(function( ) {var pieces=operator_lexer(t) ,adjust=pieces[1] .length<<2,canonical=pieces[3] ||pieces[2] || '#' ,left=adjust+ ( (base_precedence(pieces[2] ) ) || (base_precedence(canonical) ) ) ,right=adjust+ ( (base_precedence( ( (pieces[4] ) && (pieces[4] .charAt(1) ) ) ) ) || (base_precedence(canonical.charAt(canonical.length-1) ) ) ) ,unary=right===adjust||right===adjust+4,nuke=pieces[5] ,real_left=unary?1:left,real_right=unary?left:right;
 return( {l:real_left,r:real_right,id:intern(canonical) ,v:false,u:unary,n: +nuke||0} ) } ) .call(this) } ,join=parse_operator( '#' ) ,parse=function(ts) {;
-return(function( ) {var values= [ ] ,operators= [ ] ,ev=true,top=function() {;
+return(function( ) {var values= [ ] ,operators= [ ] ,ev=true,right=function(t) {;
+return! (t.l&1) ||t.id===join.id&&t.l>join.l} ,top=function() {;
 return operators[operators.length-1] } ,precedence=function(t) {;
-return( (operators.length&& ( (t.l&1?top() .r<=t.l:top() .r<t.l) ||t.n-- >0) ) && ( ( (apply() ) , (precedence(t) ) ) ) ) } ,operator=function(t) {;
+return( (operators.length&& ( (right(t) ?top() .r<t.l:top() .r<=t.l) ||t.n-- >0) ) && ( ( (apply() ) , (precedence(t) ) ) ) ) } ,operator=function(t) {;
 return( (precedence(t) ) , ( ( ! (t.i) && ( ( ( (operators) .push(t) ) , (ev=true) ) ) ) ) ) } ,observe=function(t) {;
 return t.v? ( ( ( ( ( ! (ev) && (observe(join) ) ) ) , (ev=false) ) ) , ( (values) .push(new syntax(t.id) ) ) ) :t.u&& !t.i? ( ( ( ! (ev) && (observe(join) ) ) ) , (operator(t) ) ) :operator(t) } ,apply=function() {;
 return top() .u? (values) .push(new syntax(operators.pop() .id, [values.pop() ] ) ) : (function( ) {var x=values.pop() ,y=values.pop() ;
