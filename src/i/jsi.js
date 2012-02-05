3fbac2e24973c444929435794aa5b2674ec9bf46
Spencer Tipping <spencer@spencertipping.com>
2012-02-05 07:02:58 -0700

    More library improvements, and fixed a horrible regexp serialization bug in jsi.

--- src/i/jsi.js
+++ src/i/jsi.js
@@ -7,6 +7,6 @@ xi<xl;
 return xr} ) .call(this,Array.prototype.slice.call( (this) ) ) ) } ,mh_encode_literal=function(s) {;
 return/\/./ .test(s) ? '"' +s.replace( /"/g , '\\"' ) + '"r' :s.replace( /^\$/ , '@$' ) } ,mh_encode=function(s) {;
 return/^[()\[\]{}\.,;:]/ .test(s) ?s: '/' +s} ,mh_decode_literal=function(s) {;
-return/^".*"r$/ .test(s) ? '/' +s.replace( /\\"/g , '"' ) .replace( /"r$/ , '' ) :s.replace( /^@\$/ , '$' ) } ,mh_decode=function(s) {;
+return/^".*"r$/ .test(s) ?s.replace( /^"\// , '/' ) .replace( /\\"/g , '"' ) .replace( /"r$/ , '' ) :s.replace( /^@\$/ , '$' ) } ,mh_decode=function(s) {;
 return s.replace( /^\/(.)/ , '$1' ) } ;
 return( ($) .syntax_extend( {as_mh:as_mh} ) ,$.mulholland.syntax_common.as_js=as_js) } ) .call(this) } ) ;
