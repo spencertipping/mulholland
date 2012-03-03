1ca0335d4e0c4f9825d508643c6a9d978e8ea686
Spencer Tipping <spencer@spencertipping.com>
2012-03-03 10:13:20 -0700

    Added Javascript library preloading

--- src/i/jsi.js
+++ src/i/jsi.js
@@ -1,7 +1,7 @@
 caterwaul.module( 'mulholland.jsi' ,function($) { (function( ) {var as_mh=function(p) {;
 return this.data=== '()' ?new p.syntax(p.intern( '#' ) , [this[0] .as_mh(p) ,this[1] .as_mh(p) ] ) :this.data=== '[]' ?new p.syntax(p.intern( '.' ) , [this[0] .as_mh(p) ,new p.syntax(p.intern( '[' ) , [this[1] .as_mh(p) ] ) ] ) :this.data.length===0?new p.syntax(p.intern( '@' ) ) :this.length===0?new p.syntax(p.intern(mh_encode_literal(this.data) ) ) :new p.syntax(p.intern(mh_encode_literal(this.data) ) , [this[0] .as_mh(p) ,this[1] .as_mh(p) ] ) } ,as_js=function() {;
 return this.resolved_data() === '@' ?$.empty:this.resolved_data() === '#' ?new $.syntax( '()' ,this[0] .as_js() ,this[1] .as_js() ) :represents_a_slice(this) ?new $.syntax( '[]' ,this[0] .as_js() ,this[1] [0] .as_js() ) :this.length===0?new $.syntax(mh_decode_literal(this.resolved_data() ) ) :new $.syntax(mh_decode_literal(this.resolved_data() ) , (function(xs) {var x,x0,xi,xl,xr;
-for(var xr=new xs.constructor() ,xi=0,xl=xs.length,x0,_y;
+for(var xr=new xs.constructor() ,xi=0,xl=xs.length,_y;
 xi<xl;
  ++xi)x=xs[xi] , (_y= (x.resolved_data() !== '@' &&x.as_js() ) ) &&xr.push(_y) ;
 return xr} ) .call(this,Array.prototype.slice.call( (this) ) ) ) } ,represents_a_slice=function(t) {;
